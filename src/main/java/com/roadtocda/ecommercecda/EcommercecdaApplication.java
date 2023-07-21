83f21caea8f39001af4fe605b2425f166da15930
YassineElazzati <yassine.elazzati@outlook.fr>
2023-07-21 15:39:37 +0200

    first commit

---
+++ src/main/java/com/roadtocda/ecommercecda/EcommercecdaApplication.java
@@ -0,0 +1,13 @@
+package com.roadtocda.ecommercecda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercecdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercecdaApplication.class, args);
+	}
+
+}
