80b5c2c9bb7cb807a9bbc8e1918f19396bb705d2
gaouimounir <mounir.gaoui@outlook.com>
2023-07-28 16:38:15 +0200

    pull ludo

--- src/main/java/com/roadtocda/ecommerce/cda/ecommercecdaApplication.java
+++ src/main/java/com/roadtocda/ecommerce/cda/ecommercecdaApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ecommercecdaApplication {
+public class EcommercecdaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ecommercecdaApplication.class, args);
+		SpringApplication.run(EcommercecdaApplication.class, args);
+		
 		
 		
 	}
+
 }
 
 
