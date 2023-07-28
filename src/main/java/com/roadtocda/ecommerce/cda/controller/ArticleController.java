c919259e7f7c17388813a11e77c9f7ce37415920
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-07-28 11:37:48 +0200

    feat: creat article.html and clean index.html to keep access with other pages. creation TypeArticleController to diffentiate article/Typearticle

--- src/main/java/com/roadtocda/ecommerce/cda/controller/ArticleController.java
+++ src/main/java/com/roadtocda/ecommerce/cda/controller/ArticleController.java
@@ -7,51 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.roadtocda.ecommerce.cda.model.Article;
-import com.roadtocda.ecommerce.cda.model.Taille;
-import com.roadtocda.ecommerce.cda.model.Typearticle;
 import com.roadtocda.ecommerce.cda.service.ArticleService;
-import com.roadtocda.ecommerce.cda.service.TailleService;
-import com.roadtocda.ecommerce.cda.service.TypearticleService;
 
 @Controller
 public class ArticleController {
 
 	@Autowired
 	private ArticleService ArticleService;
-	@Autowired
-	private TypearticleService typearticleService;
-	@Autowired
-	private TailleService tailleService;
 
 	
 	@GetMapping("/Articles")
 	public Iterable<Article> getArticle(){
 		return ArticleService.getArticles();
 	}
+	
 	@GetMapping("/Article")
     public String Article(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		model.addAttribute("LesArticles", ArticleService.getArticles());
-        return "index";
+        return "article";
     }
 	
-	@GetMapping("/TypeArticles")
-	public Iterable<Typearticle> getTypearticles(){
-		return typearticleService.getTypearticles();
-	}
-	
-	@GetMapping("/TypeArticle")
-	public String TypeArticle(@RequestParam(name="name", required = false, defaultValue = "ehehe") String name, Model model) {
-		model.addAttribute("name", name);
-		model.addAttribute("LesTypes", typearticleService.getTypearticles());
-		return "typearticle";
-	}
-	
-	
-	@GetMapping("/Tailles")
-	public Iterable<Taille> getTailles(){
-		return tailleService.getTailles();
-	}
 
 	
 }
